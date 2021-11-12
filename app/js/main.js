ffa43140ead77f7f966135dc801458417721434c
fluidweb <dariy.artiem@gmail.com>
2021-11-12 19:33:07 +0200

    12.11.2021 19:32

--- app/js/main.js
+++ app/js/main.js
@@ -15,24 +15,59 @@
 // closeButton.addEventListener('click', closeVideo);
 
 $(document).ready(function (){
+	let video1 = $('#home-video1')[0];
+	let videoFile = $('.video-file');
+	let videoContent = $(".video-content");
+	let background = $(".video-background");
 	$("#play1").click(function (){
-		$("#video1").css("display", "flex");
+		videoContent.animate({
+			opacity: 0
+		},500);
+		background.animate({
+			opacity: 0
+		},500);
+		videoFile.css('z-index', 1);
+		videoFile.animate({
+			opacity: 1,
+		},500);
+		$('.video-close').animate({
+			opacity: 0.7,
+		}, 500);
+		video1.play();
+		$("#close-video1").animate(function (){
+		})
 	})
-	$("#close-video1").click(function (){
-		$("#video1").css("display", "none");
-		$(".video-video")[0].pause();
+
+	$(".video-close").click(function (){
+		videoContent.animate({
+			opacity: 1
+		},500);
+		background.animate({
+			opacity: 1
+		},500);
+		videoFile.css('z-index', -1);
+		videoFile.animate({
+			opacity: 0,
+		},500);
+		$('.video-close').animate({
+			opacity: 0,
+		}, 500);
+		video1.pause();
 	})
+
 	$(".header-burger").click(function (){
 		$(".header-sidebar").animate({
 			width: '220px',
 		}, 750)
 
 	})
+
 	$(".header-closeSidebar").click(function (){
 		$(".header-sidebar").animate({
 			width: '0px',
 		}, 750)
 	})
+
 	$(".header-sidebarSearch").click(function (){
 		$(".header-absoluteSearch").animate({
 			opacity: 1,
@@ -42,6 +77,7 @@ $(document).ready(function (){
 			width: '0px',
 		}, 400)
 	})
+
 	$('.absolute-searchClose').click(function (){
 		$(".header-absoluteSearch").animate({
 			opacity: 0,
